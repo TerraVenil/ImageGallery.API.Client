81db889e27828bb829219022cb093ce958e71981
Stuart Shay <sshay@yahoo.com>
2018-07-23 17:23:21 -0400

    Code Cleanup

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
 set -e
 
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/mssql-tools/bin
-
-if [  ! -z ${MSSQL_SA_PASSWORD_FILE} ];
-then
-    export SA_PASSWORD=$(cat $MSSQL_SA_PASSWORD_FILE)
-else
-    export SA_PASSWORD=${MSSQL_SA_PASSWORD}
-fi
-
-/wait_for_it.sh -h database -p 1433 -t 0 --
-
-until /healthcheck.sh
-do
-    echo healthcheck
-    sleep 5
-done
-
-exec dotnet NavigatorIdentity.Configuration.IdentityServerDataDB.dll
+exec dotnet ImageGallery.API.Client.Console.dll
