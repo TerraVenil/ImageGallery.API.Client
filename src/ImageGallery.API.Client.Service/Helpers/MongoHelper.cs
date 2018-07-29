77374e9753ffa274c83c909ad259269938714b4c
Alexander Smirnov <panthernet@mail.ru>
2018-07-29 11:36:45 +0200

    https://github.com/stuartshay/ImageGallery.API.Client/issues/2

---
+++ src/ImageGallery.API.Client.Service/Helpers/MongoHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Serilog;
+using Serilog.Core;
+
+namespace ImageGallery.API.Client.Service.Helpers
+{
+    public static class MongoHelper
+    {
+        private static readonly ConcurrentDictionary<string, Logger> Loggers = new ConcurrentDictionary<string, Logger>();
+
+        public static bool SetupLogger(string dbPath, string collectionName = "log")
+        {
+            try
+            {
+                var log = new LoggerConfiguration()
+                    .WriteTo.MongoDB(dbPath, collectionName: collectionName)
+                    .CreateLogger();
+                Loggers.TryAdd(collectionName, log);
+            }
+            catch (Exception ex)
+            {
+                //
+                return false;
+            }
+
+            return true;
+        }
+
+        public static Logger GetLogger(string collectionName = "log")
+        {
+            return !Loggers.TryGetValue(collectionName, out var log) ? null : log;
+        }
+    }
+}
