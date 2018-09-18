524d10912f30520af61d9ed81e06cc9cf756bad6
Stuart Shay <sshay@yahoo.com>
2018-09-18 01:09:05 -0400

    Add Comments

--- src/ImageGallery.API.Client.Console/Program.cs
+++ src/ImageGallery.API.Client.Console/Program.cs
@@ -69,9 +69,12 @@ namespace ImageGallery.API.Client.Console
 
             SearchOptions photoSearchOptions = new SearchOptions
             {
+                // See for List of Available Machine Tags
+                //https://api-attractions.navigatorglass.com/swagger/#!/MachineKey/ApiMachineKeyPredicatesGet
+
                 //MachineTags = "machine_tags => nycparks:",
-                MachineTags = "machine_tags => nychalloffame:",
-                //MachineTags = "machine_tags => nycparks:m010=",
+                //MachineTags = "machine_tags => nychalloffame:",
+                MachineTags = "machine_tags => nycparks:m010=",
                 //MachineTags = "machine_tags => nycparks:m089=",
                 //MachineTags = "machine_tags => nycparks:q436=",
                 // MachineTags = "machine_tags => nycparks:m010=114",
