5f252d9c36ed31d8dddd58e355f47e0c5ff692b4
Stuart Shay <sshay@yahoo.com>
2018-10-05 15:23:24 -0400

    Add Pluging to Grapha Config

--- docker/docker-grafana-config/config/init.sh
+++ docker/docker-grafana-config/config/init.sh
@@ -11,10 +11,12 @@ fi
 
 
 ## Plugins
-grafana-cli plugins install grafana-piechart-panel
-grafana-cli plugins install novalabs-annotations-panel
-grafana-cli plugins install briangann-datatable-panel
-grafana-cli plugins install briangann-gauge-panel
+#grafana-cli plugins install grafana-piechart-panel
+#grafana-cli plugins install novalabs-annotations-panel
+#grafana-cli plugins install briangann-datatable-panel
+#grafana-cli plugins install briangann-gauge-panel
+
+
 
 ## Dashboards 
 dashboard_id=1598
