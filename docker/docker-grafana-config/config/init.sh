ae14c1551a7a3f9a033edccbba352913aaa197a3
Stuart Shay <sshay@yahoo.com>
2018-10-05 15:15:43 -0400

    Add Pluging to Grapha Config

--- docker/docker-grafana-config/config/init.sh
+++ docker/docker-grafana-config/config/init.sh
@@ -2,13 +2,21 @@
 
 set -xeuo pipefail
 
-
+## DataSource 
 if ! curl --retry 5 --retry-connrefused --retry-delay 0 -sf http://grafana:3000/api/dashboards/name/prom; then
     curl -sf -X POST -H "Content-Type: application/json" \
          --data-binary '{"name":"prom","type":"prometheus","url":"http://prometheus:9090","access":"proxy","isDefault":true}' \
          http://grafana:3000/api/datasources
 fi
 
+
+## Plugins
+grafana-cli plugins install grafana-piechart-panel
+grafana-cli plugins install novalabs-annotations-panel
+grafana-cli plugins install briangann-datatable-panel
+grafana-cli plugins install briangann-gauge-panel
+
+## Dashboards 
 dashboard_id=1598
 last_revision=$(curl -sf https://grafana.com/api/dashboards/${dashboard_id}/revisions | grep '"revision":' | sed 's/ *"revision": \([0-9]*\),/\1/' | sort -n | tail -1)
 
@@ -19,6 +27,10 @@ curl --retry-connrefused --retry 5 --retry-delay 0 -sf \
      -X POST -H "Content-Type: application/json" \
      --data-binary @data.json \
      http://grafana:3000/api/dashboards/import
+
+
+
+
 
 
 # create new user - L: viewer P:readonly
