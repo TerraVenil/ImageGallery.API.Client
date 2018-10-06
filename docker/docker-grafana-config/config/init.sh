f93d3b0f29299cc59b7e42233cb003ae27231edb
Stuart Shay <sshay@yahoo.com>
2018-10-06 15:04:44 -0400

    Update Graphana Config

--- docker/docker-grafana-config/config/init.sh
+++ docker/docker-grafana-config/config/init.sh
@@ -24,10 +24,13 @@ curl --retry-connrefused --retry 5 --retry-delay 0 -sf \
 
 
 ## Plugins TODO - Install
-grafana-cli plugins install grafana-piechart-panel
-grafana-cli plugins install novalabs-annotations-panel
-grafana-cli plugins install briangann-datatable-panel
-grafana-cli plugins install briangann-gauge-panel
+#grafana-cli plugins install grafana-piechart-panel
+#grafana-cli plugins install novalabs-annotations-panel
+#grafana-cli plugins install briangann-datatable-panel
+#grafana-cli plugins install briangann-gauge-panel
+
+
+
 
 
 
