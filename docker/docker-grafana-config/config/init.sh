a58582075a909e26f0db4f2412f3c1ba57fdcb9e
Stuart Shay <sshay@yahoo.com>
2018-10-08 11:52:31 -0400

    Update Grafana Config Docker

--- docker/docker-grafana-config/config/init.sh
+++ docker/docker-grafana-config/config/init.sh
@@ -26,6 +26,37 @@ if ! curl --retry 5 --retry-connrefused --retry-delay 0 -sf http://grafana:3000/
 fi
 
 
+# 6239 - Mysql - Prometheus
+# 6257 NFS Full - https://github.com/rfrail3/grafana-dashboards
+# 1598 - Zipkin / Prometheus
+grafana_dashboard_import () {
+
+    grafana_host="http://grafana:3000"
+    grafana_cred="admin:admin"
+    grafana_datasource="prometheus"
+    
+    ds=(6239 1598);
+
+    for d in "${ds[@]}"; do
+       echo -e "Processing $d:"
+
+        j=$(curl -s -k -u "$grafana_cred" $grafana_host/api/gnet/dashboards/$d | jq .json)
+        
+        curl -s -k -u "$grafana_cred" -XPOST -H "Accept: application/json" \
+            -H "Content-Type: application/json" \
+            -d "{\"dashboard\":$j,\"overwrite\":true, \
+                \"inputs\":[{\"name\":\"DS_PROMETHEUS\",\"type\":\"datasource\", \
+                \"pluginId\":\"prometheus\",\"value\":\"$grafana_datasource\"}]}" \
+            $grafana_host/api/dashboards/import; echo ""
+
+    done
+}
+
+
+grafana_dashboard_import;
+
+
+
 ## Dashboards 
 ## - Zipkin / Prometheus
 #dashboard_id=1598
