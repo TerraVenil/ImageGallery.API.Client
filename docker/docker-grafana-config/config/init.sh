c37246f7d4a7fcb3a3922224bc4de61c43d03ada
Stuart Shay <sshay@yahoo.com>
2018-10-09 21:09:40 -0400

    Update Grafana Config Docker

--- docker/docker-grafana-config/config/init.sh
+++ docker/docker-grafana-config/config/init.sh
@@ -1,13 +1,10 @@
-#!/bin/sh
- 
-#set -xeuo pipefail
-
+#!/bin/bash
 
 # create new user - L: viewer P:readonly
 curl --retry 5 --retry-delay 0 -sf \
     -X POST -H "Content-Type: application/json" \
     -d '{ "name":"viewer", "email":"viewer@org.com", "login":"viewer",  "password":"readonly" }' \
-    http://$grafana_cred@grafana:3000/api/admin/users 
+    http://admin:admin@grafana:3000/api/admin/users 
 
 
 # set user's home dashboard   
@@ -16,7 +13,6 @@ curl \
  -H 'Content-Type: application/json' \
  -d '{ "homeDashboardId":1 }' \
  http://viewer:readonly@grafana:3000/api/user/preferences
-
 
 
 ## DataSource 
@@ -32,7 +28,11 @@ fi
 # 1598 - Zipkin / Prometheus
 grafana_dashboard_import () {
 
-    ds=(6239 1598)
+    grafana_host="http://grafana:3000"
+    grafana_cred="admin:admin"
+    grafana_datasource="prometheus"
+    
+    ds=(6239 1598);
 
     for d in "${ds[@]}"; do
        echo -e "Processing $d:"
@@ -49,44 +49,7 @@ grafana_dashboard_import () {
     done
 }
 
-
-grafana_host="http://grafana:3000"
-grafana_cred="admin:admin"
-grafana_datasource="prometheus"
-
 grafana_dashboard_import;
-
-
-
-
-
-
-
-## Dashboards 
-## - Zipkin / Prometheus
-#dashboard_id=1598
-#last_revision=$(curl -sf https://grafana.com/api/dashboards/${dashboard_id}/revisions | grep '"revision":' | sed 's/ *"revision": \([0-9]*\),/\1/' | sort -n | tail -1)
-
-#echo '{"dashboard": ' > data.json
-#curl -s https://grafana.com/api/dashboards/${dashboard_id}/revisions/${last_revision}/download >> data.json
-#echo ', "inputs": [{"name": "DS_PROMETHEUS", "pluginId": "prometheus", "type": "datasource", "value": "prom"}], "overwrite": false}' >> data.json
-#curl --retry-connrefused --retry 5 --retry-delay 0 -sf \
-#     -X POST -H "Content-Type: application/json" \
-#     --data-binary @data.json \
-#     http://grafana:3000/api/dashboards/import
-
-
-#-  Mysql - Prometheus  - 6239
-#dashboard_id_1=6239
-
-
-
-
-
-
-
-
-
 
 
 
